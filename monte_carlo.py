3d9438ac578286fb0a1ddc67f9fcd399481b9b5b
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-09-23 16:34:45 -0700

    Refactor Monte Carlo simulation functions and increase iterations

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,6 +1,6 @@
 from random import random
 
-def prob_char(p: int):
+def prob(p: int):
     if p <= 0:
         return 0
     elif p <= 73:
@@ -10,17 +10,7 @@ def prob_char(p: int):
     else:
         return 1
 
-def prob_weap(p: int):
-    if p <= 0:
-        return 0
-    elif p <= 62:
-        return 0.007
-    elif p <= 76:
-        return 0.007 + 0.07 * (p - 62)
-    else:
-        return 1
-
-def run_char():
+def run():
     pity_flag = False
     c = 0
     n = 1
@@ -36,14 +26,11 @@ def run_char():
         else:
             n += 1
 
-def run_weap():
-    
-
 def loop(n_iter):
     d = dict()
 
     for _ in range(n_iter):
-        i = run()
+        i = run() + run() + run() + run() + run() + run() + run() + run() + run() + run() + run() + run()
         if i in d.keys():
             d[i] += 1
         else:
@@ -57,4 +44,4 @@ def write(n_iter):
         for key in sorted(data.keys()):
             file.write(f"{key}: {data[key]}\n")
 
-write(10_000)
+write(1_000_000)
